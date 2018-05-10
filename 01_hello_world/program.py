a6795b9852bfc54c07aa7b720cbc0bb4d590987d
Yang Dai <yangdai713@aim.com>
2018-05-10 18:04:33 -0400

    initial commit

---
+++ 01_hello_world/program.py
@@ -0,0 +1,9 @@
+print('--------------------------------------------------')
+print('                   HELLO APP')
+print('--------------------------------------------------')
+print()
+
+user_text = input('What is your name? ')
+greeting = 'Nice to meet you {0}'.format(user_text)
+
+print(greeting)
---
+++ birthday_countdown/program.py
@@ -0,0 +1,25 @@
+def print_header():
+    pass
+
+
+def get_birthday_from_user():
+    pass
+
+
+def compute_days_between_dates():
+    pass
+
+
+def print_birthday_information():
+    pass
+
+
+def main():
+    """
+
+    :return:
+    """
+    print_header()
+    bday = get_birthday_from_user()
+
+    compute_days_between_dates(bday, now )
